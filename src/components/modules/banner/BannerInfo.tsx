a684b6a73642006b6b458598b6e8c97e268d3e79
SlavikAir <slavikair2022@gmail.com>
2023-09-19 13:28:00 +0300

    fix: banner text

--- src/components/modules/banner/BannerInfo.tsx
+++ src/components/modules/banner/BannerInfo.tsx
@@ -1,9 +1,14 @@
-const info = "Diligent, determined. With a strong desire to learn and master all the intricacies of programming. Open to criticism, always willing to listen to others' opinions."
+const info = "If you need a resume in pdf format"
+const info0 = "it can be downloaded by clicking the button"
+
+ 
 
 export const BannerInfo = () => {
     return(
         <div className="banner-info">
-            <p className="content">{info}</p>
+            <p className="content-banner">Hello !</p>
+            <p className="content-banner">{info}</p>
+            <p className="content-banner">{info0}</p>
             <button className="button">download</button>
         </div>
     )
