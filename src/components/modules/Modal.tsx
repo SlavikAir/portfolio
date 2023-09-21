e414f5aed16ec33c274cf4438848d4ec65a908d1
SlavikAir <slavikair2022@gmail.com>
2023-09-21 23:33:36 +0300

    fix css

--- src/components/modules/Modal.tsx
+++ src/components/modules/Modal.tsx
@@ -1,34 +1,28 @@
 import { MouseEvent } from "react";
-import { FC } from "react"
-
-
+import { FC } from "react";
 
 interface IModal {
-    modal:boolean,
-    close:any
-    
+  modal: boolean;
+  close: any;
 }
 
-
-
-export const Modal:FC<IModal> = ({modal,close}) => {
-
-
-    const handleClick = (event: MouseEvent<HTMLDivElement>) => {
-        event.stopPropagation()
-        console.log(event.currentTarget.id)
-                if (event.currentTarget.id !== 'modalDiv') {
-          return
-        }
-        close()
-      };
-    return (
-        <div id="modalDiv"className={`modal ${modal ? 'open' : 'close'}`} onClick={handleClick}> 
-                <div className="mail-modal-body">
-                    <div className='modal-body'>
-                            <p>Slavikair2022@gmail.com</p>
-                    </div>
-             </div>
+export const Modal: FC<IModal> = ({ modal, close }) => {
+  const handleClick = (event: MouseEvent<HTMLDivElement>) => {
+    if (event.target === event.currentTarget) {
+      close();
+    }
+  };
+  return (
+    <div
+      id="modalDiv"
+      className={`my-modal ${modal ? "open" : "close"}`}
+      onClick={handleClick}
+    >
+      <div className="mail-modal-body">
+        <div className="modal-body">
+          <p>Slavikair2022@gmail.com</p>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
